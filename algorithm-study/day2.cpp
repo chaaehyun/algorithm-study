da80756e3c52f711736d857b09c7297d7e66f552
chaaehyun <connie6301@naver.com>
2024-08-19 17:56:45 +0900

    day2

--- algorithm-study/day2.cpp
+++ algorithm-study/day2.cpp
@@ -1,3 +1,35 @@
 // 백준 1058 요세푸스
 
 #include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int n;
+int k;
+queue<int> q;
+
+int main() {
+	cin >> n >> k;
+
+	for (int i = 1; i <= n; i++) {
+		q.push(i);
+	}
+
+	cout << "<";
+	while (!q.empty()) {
+		for (int i = 0; i < k-1; i++) {
+			q.push(q.front());
+			q.pop();
+		}
+
+		cout << q.front();
+		q.pop();
+
+		if (!q.empty())
+			cout << ", ";
+	}
+	cout << ">";
+	return 0;
+}

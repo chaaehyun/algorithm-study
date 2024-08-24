57fd3c8a301dcece94b4cba1f9d4d7598887ce31
chaaehyun <connie6301@naver.com>
2024-08-24 13:09:57 +0900

    1 week upadates

--- algorithm-study/day1.cpp
+++ algorithm-study/day1.cpp
@@ -1,70 +1,70 @@
 // 백준 10828번 스택
 
-#include <iostream>
-#include <stack>
-#include <string>
-#include <algorithm>
-
-using namespace std;
-
-int n;
-int x;
-stack<int> s;
-
-void push(int x) {
-	s.push(x);
-}
-
-void pop() {
-	if (!s.empty())
-	{
-		cout << s.top() << endl;
-		s.pop();
-	}
-
-	else
-		cout << -1 << endl;
-}
-
-void size() {
-	cout << s.size() << endl;
-}
-
-void empty() {
-	cout << s.empty() << endl;
-}
-
-void top() {
-	if (!s.empty())
-		cout << s.top() << endl;
-
-	else
-		cout << -1 << endl;
-}
-
-int main() {
-	string order;
-	cin >> n;
-
-	for (int i = 1; i <= n; i++) {
-		cin >> order;
-		if (order == "push") {
-			cin >> x;
-			push(x);
-		}
-
-		else if (order == "pop")
-			pop();
-
-		else if (order == "size")
-			size();
-
-		else if (order == "empty")
-			empty();
-
-		else
-			top();
-	}
-
-	return 0;
-}
+//#include <iostream>
+//#include <stack>
+//#include <string>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int n;
+//int x;
+//stack<int> s;
+//
+//void push(int x) {
+//	s.push(x);
+//}
+//
+//void pop() {
+//	if (!s.empty())
+//	{
+//		cout << s.top() << endl;
+//		s.pop();
+//	}
+//
+//	else
+//		cout << -1 << endl;
+//}
+//
+//void size() {
+//	cout << s.size() << endl;
+//}
+//
+//void empty() {
+//	cout << s.empty() << endl;
+//}
+//
+//void top() {
+//	if (!s.empty())
+//		cout << s.top() << endl;
+//
+//	else
+//		cout << -1 << endl;
+//}
+//
+//int main() {
+//	string order;
+//	cin >> n;
+//
+//	for (int i = 1; i <= n; i++) {
+//		cin >> order;
+//		if (order == "push") {
+//			cin >> x;
+//			push(x);
+//		}
+//
+//		else if (order == "pop")
+//			pop();
+//
+//		else if (order == "size")
+//			size();
+//
+//		else if (order == "empty")
+//			empty();
+//
+//		else
+//			top();
+//	}
+//
+//	return 0;
+//}
--- algorithm-study/day3.cpp
+++ algorithm-study/day3.cpp
@@ -1,38 +1,38 @@
 //프로그래머스 다리를 지나는 트럭
-#include <string>
-#include <vector>
-#include <queue>
-
-using namespace std;
-
-int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    int answer = 0;
-    queue<int> q;
-    int index = 0;
-    int sum = 0;
-    int car = 0;
-
-    while (1) {
-        if (index == truck_weights.size()) {
-            answer += bridge_length;
-            break;
-        }
-
-        answer+=1;
-        car = truck_weights[index];
-
-        if (sum + car <= weight) {
-            q.push(car);
-            sum += car;
-            index++;
-        }
-        else
-            q.push(0);
-
-        if (q.size() == bridge_length) {
-            sum -= q.front();
-            q.pop();
-        }
-    }
-    return answer;
-}
+//#include <string>
+//#include <vector>
+//#include <queue>
+//
+//using namespace std;
+//
+//int solution(int bridge_length, int weight, vector<int> truck_weights) {
+//    int answer = 0;
+//    queue<int> q;
+//    int index = 0;
+//    int sum = 0;
+//    int car = 0;
+//
+//    while (1) {
+//        if (index == truck_weights.size()) {
+//            answer += bridge_length;
+//            break;
+//        }
+//
+//        answer+=1;
+//        car = truck_weights[index];
+//
+//        if (sum + car <= weight) {
+//            q.push(car);
+//            sum += car;
+//            index++;
+//        }
+//        else
+//            q.push(0);
+//
+//        if (q.size() == bridge_length) {
+//            sum -= q.front();
+//            q.pop();
+//        }
+//    }
+//    return answer;
+//}

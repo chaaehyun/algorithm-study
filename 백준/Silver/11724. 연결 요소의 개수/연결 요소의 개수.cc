25a06bf9b442f3012e232e8f98170c8110e1812a
Chaaehyun <81129852+chaaehyun@users.noreply.github.com>
2024-08-28 23:15:36 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 244 ms, Memory: 6404 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -9,7 +9,7 @@ int n, m;
 void DFS(int x)
 {
     visited[x] = 1;
-    for (int i = 0; i < vec[x].size(); i++) //최댓값 주의 : 벡터 그 원소에 해당하는 크기만큼 돌아야함
+    for (int i = 0; i < vec[x].size(); i++)
     {
         int idx = vec[x][i];
         if (visited[idx] == 0)
